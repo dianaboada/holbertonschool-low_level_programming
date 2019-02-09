b8b28b365f4e5ef479181c1a194784ddce0c0742
dianaboada <dcboadab@gmail.com>
2019-02-09 00:59:00 +0000

    A C program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Source code to print the last digit of the number stored in the variable n
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
