a7dc88b3ec2157aedb1e573b14ec697781a748d4
dianaboada <dcboadab@gmail.com>
2019-02-10 04:02:25 +0000

    a C program that prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int decena1, unidad1, decena2, unidad2;
+
+	for (decena1 = 48 ; decena1 <= 57; decena1++)
+	{
+		for (unidad1 = 48; unidad1 <= 57; unidad1++)
+		{
+			for (decena2 = 48 ; decena2 <= 57; decena2++)
+			{
+				for (unidad2 = 48 ; unidad2 <= 57 ; unidad2++)
+				{
+					if (decena1 + unidad1 > decena2 + unidad2)
+						continue;
+					if (decena1 + unidad1 == decena2 + unidad2)
+						continue;
+
+					putchar(decena1);
+					putchar(unidad1);
+					putchar(32);
+					putchar(decena2);
+					putchar(unidad2);
+					if (decena1 + unidad1 + 32 + decena2 + unidad2 < 259)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
