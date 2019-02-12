8af55d4776a7e294fa3f2278a084ff6835119a14
dianaboada <dcboadab@gmail.com>
2019-02-12 16:01:42 +0000

    A program that prints Holberton, followed by a new line. With the function _putchar()

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,9 +1,16 @@
-#include <stdio.h>
+#include "holberton.h"
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 
+_putchar('\n');
 return (0);
 }
-
-
