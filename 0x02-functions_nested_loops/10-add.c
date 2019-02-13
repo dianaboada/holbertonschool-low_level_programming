9baf0b1da5cadee7e424d8a86148d8e9b48c0d38
dianaboada <dcboadab@gmail.com>
2019-02-13 03:45:26 +0000

    A function that adds two integers(saved in var1 and var2) and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* add - check the code for Holberton School students.
+*
+* @var1: adds a integer and returns the result.
+* @var2: adds a integer and returns the result.
+*
+* Return: Always 0.
+*/
+int add(int var1, int var2)
+{
+	int sum;
+
+	sum = var1 + var2;
+	return (sum);
+}
