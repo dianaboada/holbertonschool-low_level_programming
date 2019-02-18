ab31758491c6fea8ab5009481b0089558fad6cd9
dianaboada <dcboadab@gmail.com>
2019-02-17 23:24:57 -0500

    A function that prints an integer.

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+* print_number - A function that prints an integer.
+* @n: is the integer that program should print.
+*
+* Return: Void.
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	int decimal = n;
+	int times_digit = 0;
+
+	while (decimal / 10 > 0)
+	{
+		times_digit++;
+		decimal = decimal / 10;
+	}
+
+	int expo = (int)pow(10, times_digit);
+	int i;
+
+	for (i = 0; i <= times_digit; i++)
+	{
+		_putchar(n / expo + '0');
+		n = n % expo;
+		expo = expo / 10;
+	}
+}
+
