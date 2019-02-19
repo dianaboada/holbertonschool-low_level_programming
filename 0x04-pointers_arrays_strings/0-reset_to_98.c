b583684cfd821654cb1a31113c8d0877f71a24fc
dianaboada <dcboadab@gmail.com>
2019-02-19 23:05:51 +0000

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - a function that takes a *ip as parameter an reset it with 98.
+* @*n: Parameters.
+*
+* Return: Void.
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
