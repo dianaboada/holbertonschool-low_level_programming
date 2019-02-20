c3763b0ee09858950800d3f23ee606cfaafeeccf
dianaboada <dcboadab@gmail.com>
2019-02-20 22:43:44 +0000

    A function that reverses a string.

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -8,23 +8,21 @@
 */
 void rev_string(char *s)
 {
-	int a = 0;
-	int b;
-	int c;
-	char d;
-	int e = 0;
+	int forward = 0; /*Initial position looking forward 'H'*/
+	int backward; /*Initial position looking backward 'n'*/
+	int aux; /*backup variable, host the value to be replaced on each position*/
 
-	while (s[a] != '\0')
+	for (backward = 0 ; s[backward] != '\0'; backward++)
 	{
-		a++;
+
 	}
-		c = a / 2;
-
-	for (b = a - 1; b >= c; b--)
+	backward--;
+	while (forward < backward)
 	{
-		d = s[b];
-		s[b] = s[e];
-		s[e] = d;
-		e++;
+		aux = s[forward];
+		s[forward] = s[backward];
+		s[backward] = aux;
+		forward++;
+		backward--;
 	}
 }
