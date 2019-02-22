321eb4846e9455f52cf71ca46122b088174c74ac
dianaboada <dcboadab@gmail.com>
2019-02-22 17:39:04 -0500

    A function that copies 2 strings

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -2,24 +2,18 @@
 #include <stdio.h>
 
 /**
-* *_strncpy - function that copies a string..
+* *_strncpy - function that copies a string.
 * @dest: saves string1.
 * @src: saves string2.
 * @n: entry.
 *
 * Return: dest
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int string1 = 0;
-	int string2 = 0;
+        int iterator;
 
-	for (string1 = 0; dest[string1] != '\0'; string1++)
-	{
-	}
-	for (string2 = 0; src[string2] != '\0' && string2 < n ; string2++)
-	{
-		dest[string1 + string2] = src[string2];
-	}
-	return (dest);
+        for (iterator = 0 ; iterator < n && dest[iterator] != '\0' ; iterator++)
+                dest[iterator] = src[iterator];
+        return (dest);
 }
