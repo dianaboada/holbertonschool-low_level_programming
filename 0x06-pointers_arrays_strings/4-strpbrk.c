d3ace09d80c4135e4f8ef9efbdbf17230a5129bf
dianaboada <dcboadab@gmail.com>
2019-02-26 02:55:33 +0000

    Function that searches a string for any of a set of bytes.

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -12,18 +12,21 @@ char *_strpbrk(char *s, char *accept)
 	int index_s;
 	int index_accept;
 	int index_equal = 0;
+	int flag = 0;
 
-	for (index_s = 0; s[index_s] != '\0' ; index_s++)
+	for (index_s = 0; s[index_s] != '\0'; index_s++)
 	{
 		for (index_accept = 0; accept[index_accept] != '\0'; index_accept++)
 		{
 			if (s[index_s] == accept[index_accept])
 			{
 				index_equal = index_s;
-				index_s = 99;
+				flag = 1;
 				break;
 			}
 		}
+		if (flag == 1)
+			break;
 	}
 	return (&s[index_equal]);
 }
