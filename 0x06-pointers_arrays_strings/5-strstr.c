6c91e1addbd6e3ad4f4ba98cbf1145d5f4bc2cb8
dianaboada <dcboadab@gmail.com>
2019-02-26 23:59:44 -0500

    Function that locates a substring.

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,44 +1,44 @@
 #include "holberton.h"
 
 /**
-  * *_strstr - Function that locates a substring.
-  * @haystack: string where to search.
-  * @needle: string to search.
-  *
-  * Return: char.
-  */
+* *_strstr - Function that locates a substring.
+* @haystack: string where to search.
+* @needle: string to search.
+*
+* Return: char.
+*/
 char *_strstr(char *haystack, char *needle)
 {
-        int i_needle;
-        int i_haystack;
-        int flag = 0;
-        int i_final = 0;
-        int flag1 = 0;
+	int i_needle;
+	int i_haystack;
+	int flag = 0;
+	int i_final = 0;
+	int flag1 = 0;
 
-        for (i_needle = 0; needle[i_needle] != '\0'; i_needle++)
-        {
-                for (; haystack[i_haystack] != '\0'; i_haystack++)
-                {
-                        if (needle[i_needle] == haystack[i_haystack])
-                        {
-                                flag = 1;
-                                if (flag1 == 0)
-                                {
-                                        i_final = i_haystack;
-                                        flag1 = 1;
-                                }
-                                break;
-                        }
-                        else
-                        {
-                                flag1 = 0;
-                                i_needle = 0;
-                        }
-                }
-                if (flag == 1)
-                        continue;
-                else
-                        return ('\0');
-        }
-        return (&haystack[i_final]);
+	for (i_needle = 0; needle[i_needle] != '\0'; i_needle++)
+	{
+		for (; haystack[i_haystack] != '\0'; i_haystack++)
+		{
+			if (needle[i_needle] == haystack[i_haystack])
+			{
+				flag = 1;
+				if (flag1 == 0)
+				{
+					i_final = i_haystack;
+					flag1 = 1;
+				}
+				break;
+			}
+			else
+			{
+				flag1 = 0;
+				i_needle = 0;
+			}
+		}
+		if (flag == 1)
+			continue;
+		else
+			return ('\0');
+		}
+	return (&haystack[i_final]);
 }
