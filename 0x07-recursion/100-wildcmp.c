72240defd261e4096eeb3bb71efc2adbd9f7eec5
dianaboada <dcboadab@gmail.com>
2019-02-28 16:42:28 -0500

    Function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* wildcmp - Function that prints a string in reverse.
+* @s1: String.
+* @s2: Strin2 that contains '*'.
+*
+* Return: int.
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2  && *s1 != '\0' && *s2 == '\0')
+	return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 == '\0')
+	return (1);
+	if (*s2 == '*')
+	{
+	if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+	return (1);
+	if (*(s1 + 1) != '\0' && *s2 == '\0')
+	return (0);
+	}
+	return (0);
+}
