9752d3a499095b3e7f5e18f1b383f1b9708f4312
dianaboada <dcboadab@gmail.com>
2019-03-02 23:44:28 -0500

    script create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -9,36 +9,36 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int i_needle;
-	int i_haystack;
-	int flag = 0;
-	int i_final = 0;
-	int flag1 = 0;
+	int i_needle;/*Index(posicion de cada letra) de la palabra que busco, substring*/
+	int i_haystack;/*Index de la frase donde busco la palabra string*/
+	int flag = 0;/*encontre la posible letra*/
+	int i_final = 0;/*Donde guardo la posicion incial de la palabra en el pajar*/
+	int flag1 = 0;/*encontre la primera letra de la palabra esta si es*/
 
-	for (i_needle = 0; needle[i_needle] != '\0'; i_needle++)
+	for (i_needle = 0; needle[i_needle] != '\0'; i_needle++) /*recorro la aguja en el pajar lo que estoy buscando*/
 	{
-		for (; haystack[i_haystack] != '\0'; i_haystack++)
+		for (; haystack[i_haystack] != '\0'; i_haystack++) /*el pajar, donde estoy buscando*/
 		{
-			if (needle[i_needle] == haystack[i_haystack])
-			{
-				flag = 1;
-				if (flag1 == 0)
+			if (needle[i_needle] == haystack[i_haystack])/*comparo la letra de la palabra que busco con la letra de donde estoy buscando*/
+			{ /*-->si lo encuentro creo una bandera*/
+				flag = 1;/*encontre un match*/
+				if (flag1 == 0)/*encontre la primera letra que necesito*/
 				{
-					i_final = i_haystack;
-					flag1 = 1;
+					i_final = i_haystack;/*si es la primera letra le asigno esa posicion a la variable que voy a usar para retornar i_final*/
+					flag1 = 1;/*Cambio el valor para que no vuelva a entrar al if no me cambie el indice de la letra que encontre*/
 				}
-				break;
+				break;/*Me salgo del pajar, del for interno*/
 			}
 			else
 			{
-				flag1 = 0;
-				i_needle = 0;
+				flag1 = 0;/*reinicia la busqueda de la palabra y que vuelva a buscar la primera letra, ees decir que la que encontro es falsa*/
+				i_needle = 0;/*reinicia la busqueda desde el principio, vuelvo a buscar la primera letra de mi palabra*/
 			}
 		}
-		if (flag == 1)
-			continue;
+		if (flag == 1)/*-->primer bandera que me va a servir para seguir buscando, si ya encontre la letra que estoy buscando paso a la siguiente con el continue*/
+			continue;/*no he terminado de encontrar todas las letras de mi palabra, sigo a la siguiente*/
 		else
-			return ('\0');
+			return ('\0');/*termine el pajar y no encontre todas las letras de mi palabra, entonces retorno NULL*/
 		}
-	return (&haystack[i_final]);
+	return (&haystack[i_final]);/*la aguja, retorno exactamente la palabra que busco*/
 }
