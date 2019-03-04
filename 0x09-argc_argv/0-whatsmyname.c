e2573de21c138eab73f370db5e886a101950cfc2
dianaboada <dcboadab@gmail.com>
2019-03-04 01:48:52 +0000

    Program that prints its name, followed by a new line.

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
 * main - Program that prints its name, followed by a new line.
 * @argc: Number of command line arguments.
-* @argv[]: Array containing the program command line arguments.
+* @argv: Array containing the program command line arguments.
 *
 * Return: 0.
 */
