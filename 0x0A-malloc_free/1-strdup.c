2c98118e1c750285f602fc359115f9c25c0321b5
dianaboada <dcboadab@gmail.com>
2019-03-06 03:42:25 +0000

    Function returns a pointer to a new string which is a duplicate of the string str.

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *_strdup - Function returns a pointer to a new string.
+* @str: string.
+*
+* Return: array.
+*/
+char *_strdup(char *str)
+{
+	int size;
+	char *cpy_array;
+	int index;
+
+	for (size = 0; str[size] != '\0' ; size++)
+	{
+
+	}
+	cpy_array = malloc(sizeof(char) * size);
+	if (cpy_array == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+	{
+		cpy_array[index] = str[index];
+	}
+	return (cpy_array);
+}
