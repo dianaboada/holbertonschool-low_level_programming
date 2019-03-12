4bf638b84b314be71bccc299f71e6d8ff4e3594a
dianaboada <dcboadab@gmail.com>
2019-03-12 01:02:46 +0000

    A struct dog with name, age and owner elements

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Data structure of name, age, owner.
+ * @name: Dogs's name.
+ * @age: Age's dog.
+ * @owner: Owner's dog.
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
